5cf35db173d0cab58752ec9b221dfa0840882cff
Nathan Fardin <nathan.fardin@gmail.com>
2025-02-06 18:17:52 +0100

    Ajout du fichier de base pour l'execution

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+#Configuration du fichier de gestion
+st.set_page_config(page_title="Prêt à dépenser", layout="wide")
+
+st.title("Application de Prédiction de Capacité de Paiement")
+st.write("Utilisez le menu latéral pour naviguer entre les différentes pages de l'application.")
